3b4f7dd07f5cf28e9bf185c92ba2d7a7dba18e2e
Lia-Peterson <liapeterson04@gmail.com>
2022-05-26 14:21:26 -0500

    added basic models (customer, admin, product) and adjusted views.py to match the project. attempted to def collection and buy in views

--- polls/views.py
+++ polls/views.py
@@ -2,27 +2,32 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 
-from .models import Question
+from. models import Customer
+from. models import Admin
+from. models import Product
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
+    latest_product_list = Product.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.product_text for p in latest_product_list])
     return HttpResponse(output)
 
+
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_product_list = Product.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
-    context = {'latest_question_list': latest_question_list,}
+    context = {'latest_product_list': latest_product_list,}
     return HttpResponse(template.render(context, request))
 # goes to template aka html
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+def detail(request, product_id):
+    return HttpResponse("You're looking at product %s." % product_id)
 # response method (ex. 404)
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+def collection(request, product_id):
+    latest_product_list = Product.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_product_list': latest_product_list,}
+    return HttpResponse("This is the first collection" % template.render(context, request))
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+def buy (request, product_id):
+    return HttpResponse("%s has been added to your cart." % product_id)
