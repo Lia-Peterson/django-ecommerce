43f448150a654f7847ccf96146c89492c73b9399
Lia-Peterson <liapeterson04@gmail.com>
2022-05-31 13:34:39 -0500

    adding products 5/27

--- polls/urls.py
+++ polls/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results', views.results, name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote')
+    path('<int:question_id>/buy/', views.buy, name='buy')
 ]
 
 #views.detail = business logic 
